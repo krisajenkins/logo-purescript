3d1ec98a7c0ec8b44c4219d05eefc86650120692
Kris Jenkins <krisajenkins@gmail.com>
2017-05-10 21:28:03 +0100

    A few small changes.

--- src/Logo.purs
+++ src/Logo.purs
@@ -1,22 +1,71 @@
 module Logo where
 
 import Prelude
-import Control.Monad.RWS (RWS, tell)
+import Control.Monad.Free.Trans (FreeT, liftFreeT, runFreeT)
+import Control.Monad.RWS (tell)
+import Control.Monad.Rec.Class (class MonadRec)
+import Control.Monad.State (class MonadState)
+import Control.Monad.Writer (WriterT, execWriter)
+import Control.Monad.Writer.Trans (class MonadTell)
+import Data.Identity (Identity)
 import Data.Lens (assign, lens, modifying, use)
 import Data.Lens.Types (Lens')
 import Math (Radians, cos, pi, sin)
 
-data Turtle = Turtle
+------------------------------------------------------------
+-- The Monad.
+------------------------------------------------------------
+
+data TurtleF a
+  = Forward Number a
+  | TurnLeft Degrees a
+  | TurnRight Degrees a
+
+type TurtleT m a = FreeT TurtleF m a
+type Turtle a = FreeT TurtleF Identity a
+
+derive instance turtleTFunctor :: Functor TurtleF
+derive instance turtleTEq :: Eq a => Eq (TurtleF a)
+
+
+forward :: forall m. Monad m => Number -> FreeT TurtleF m Unit
+forward n = liftFreeT $ Forward n unit
+
+turnRight :: forall m. Monad m => Number -> TurtleT m Unit
+turnRight degrees = liftFreeT $ TurnRight degrees unit
+
+------------------------------------------------------------
+-- The toString interpreter.
+------------------------------------------------------------
+
+logCommands :: forall m a. MonadTell (Array String) m => TurtleF a -> m a
+logCommands (Forward n next) = do
+  tell [ "Forward " <> show n ]
+  pure next
+logCommands (TurnLeft n next) = do
+  tell [ "TurnLeft " <> show n ]
+  pure next
+logCommands (TurnRight n next) = do
+  tell [ "TurnRight " <> show n ]
+  pure next
+
+runToStrings :: forall a. FreeT TurtleF (WriterT (Array String) Identity) a -> Array String
+runToStrings =
+  execWriter <<< runFreeT logCommands
+
+------------------------------------------------------------
+-- The to "thing that can be easily rendered" interpreter.
+------------------------------------------------------------
+
+data TurtleState = TurtleState
   { angle :: Radians
   , position :: Position
   }
 
-data Action = Line Position Position
+type Degrees = Number
 
-type TurtleM = RWS Unit (Array Action) Turtle
-
-initialTurtle :: Turtle
-initialTurtle = Turtle
+initialState :: TurtleState
+initialState = TurtleState
   { angle: 0.0
   , position: Position { x: 0.0, y: 0.0 }
   }
@@ -36,37 +85,15 @@ _y = lens get set
   where get (Position {y}) = y
         set (Position position) y = Position $ position {y = y}
 
-_position :: Lens' Turtle Position
+_position :: Lens' TurtleState Position
 _position = lens get set
-  where get (Turtle {position}) = position
-        set (Turtle turtle) position = Turtle $ turtle {position = position}
+  where get (TurtleState {position}) = position
+        set (TurtleState turtle) position = TurtleState $ turtle {position = position}
 
-
-_angle :: Lens' Turtle Radians
+_angle :: Lens' TurtleState Radians
 _angle = lens get set
-  where get (Turtle {angle}) = angle
-        set (Turtle turtle) angle = Turtle $ turtle {angle = angle}
-
-forward :: Number -> TurtleM Unit
-forward n = do
-  startPosition <- use _position
-  startAngle <- use _angle
-  let endPosition = move n startAngle startPosition
-  assign _position endPosition
-  tell [ Line startPosition endPosition ]
-  pure unit
-
-right :: Degrees -> TurtleM Unit
-right delta = do
-  modifying _angle ((+) (degreesToRadians delta))
-  pure unit
-
-left :: Degrees -> TurtleM Unit
-left delta = do
-  modifying _angle ((-) (degreesToRadians delta))
-  pure unit
-
-type Degrees = Number
+  where get (TurtleState {angle}) = angle
+        set (TurtleState turtle) angle = TurtleState $ turtle {angle = angle}
 
 degreesToRadians :: Degrees -> Radians
 degreesToRadians =
@@ -78,3 +105,25 @@ move distance angle (Position {x, y}) =
   where
     newX = cos angle * distance
     newY = sin angle * distance
+
+data Drawing = Line Position Position
+
+toDrawings :: forall m a. MonadState TurtleState m => MonadTell (Array Drawing) m => TurtleF a -> m a
+toDrawings (Forward n next) = do
+  startPosition <- use _position
+  startAngle <- use _angle
+  let endPosition = move n startAngle startPosition
+  assign _position endPosition
+  tell [ Line startPosition endPosition ]
+  pure next
+
+toDrawings (TurnRight delta next) = do
+  modifying _angle ((+) (degreesToRadians delta))
+  pure next
+
+toDrawings (TurnLeft delta next) = do
+  modifying _angle ((-) (degreesToRadians delta))
+  pure next
+
+runToDrawings :: forall a m. MonadRec m => MonadState TurtleState m => MonadTell (Array Drawing) m => TurtleT m a -> m a
+runToDrawings = runFreeT toDrawings
--- src/Main.purs
+++ src/Main.purs
@@ -1,21 +1,23 @@
 module Main where
 
 import Prelude
-import Halogen.Component (Component, ComponentDSL, ComponentHTML, component)
+import Data.Array as Array
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Ref (REF)
-import Control.Monad.RWS (RWSResult(..), runRWS)
+import Control.Monad.State (execStateT)
+import Control.Monad.Writer (execWriter)
 import Control.Safely (replicateM_)
 import DOM (DOM)
 import Data.Maybe (Maybe(..))
 import Halogen.Aff (awaitBody, runHalogenAff)
-import Halogen.HTML (HTML, div_, h1_, text)
+import Halogen.Component (Component, ComponentDSL, ComponentHTML, component)
+import Halogen.HTML (HTML, div_, h1_, li_, text)
 import Halogen.VDom.Driver (runUI)
-import Logo (Position(..), Action(..), TurtleM, forward, initialTurtle, right)
+import Logo (Drawing(..), Position(..), TurtleT, forward, initialState, runToDrawings, runToStrings, turnRight)
 import Svg (height, line, stroke, strokeWidth, svg, viewBox, width, x1, x2, y1, y2)
 
 main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR, dom :: DOM, exception :: EXCEPTION, ref :: REF | eff) Unit
@@ -51,31 +53,33 @@ render state =
           , viewBox "-150 -150 300 300"
           ]
           drawing1Group
+    , div_ (li_ <<< Array.singleton <<< text <$> drawingLog)
     ]
   where
-    (RWSResult state result writer) = runRWS drawing1 unit initialTurtle
     drawing1Group :: forall p i. Array (HTML p i)
-    drawing1Group = map toSvgTag writer
-    toSvgTag :: forall p i. Action -> HTML p i
-    toSvgTag (Line (Position from) (Position to)) =
-      line [ x1 from.x
-           , y1 from.y
-           , x2 to.x
-           , y2 to.y
-           , strokeWidth 2
-           , stroke "black"
-           ]
-           []
+    drawing1Group = map toSvgTag $ execWriter $ execStateT (runToDrawings drawing1) initialState
+    drawingLog = runToStrings drawing1
 
-drawing1 :: TurtleM Unit
+toSvgTag :: forall p i. Drawing -> HTML p i
+toSvgTag (Line (Position from) (Position to)) =
+  line [ x1 from.x
+       , y1 from.y
+       , x2 to.x
+       , y2 to.y
+       , strokeWidth 2
+       , stroke "black"
+       ]
+       []
+
+drawing1 :: forall m. Monad m => TurtleT m Unit
 drawing1 = replicateM_ 6 $ do
-  right 60.0
+  turnRight 60.0
   star
 
-star :: TurtleM Unit
+star :: forall m. Monad m => TurtleT m Unit
 star = replicateM_ 5 starPart
 
-starPart :: TurtleM Unit
+starPart :: forall m. Monad m => TurtleT m Unit
 starPart = do
   forward 100.0
-  right 144.0
+  turnRight 144.0
